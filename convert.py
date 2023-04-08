6a4730e200bdfdae1d656ba5e184bad9890ec8e3
tatanoi <ta.phasin@gmail.com>
2023-04-08 20:44:46 +0700

    first commit

---
+++ convert.py
@@ -0,0 +1,29 @@
+to_remov = {"A": "4", "E": "3", "I": "1", "S": "5", "T" : "7", "G" : "9", "O" : "0", "R" : "2"}
+to_Tran =  {"4": "A", "3": "E", "1": "I", "5": "S", "7" : "T", "9" : "G", "0" : "O", "2" : "R"}
+
+def Convert(string):
+    genstr = string
+    for char in to_remov.keys():
+        Nstr = genstr.replace(char, to_remov[char])
+        genstr = Nstr
+    return Nstr
+
+def Translate(string):
+    genstr = string
+    for char in to_Tran.keys():
+        Nstr = genstr.replace(char, to_Tran[char])
+        genstr = Nstr
+    return Nstr
+
+print("Translate(T) or Convert(C) :")
+func = input()
+
+print("input : ")
+string = input()
+string = string.upper()
+print(string)
+
+if func == "T":
+    print("Altered string: " +Translate(string))
+elif func == "C":
+    print("Altered string: " +Convert(string))
